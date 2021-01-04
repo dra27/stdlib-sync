3cc14d28b41131f55b79cc2110156c19b4ae9c0c
John Whitington <john@coherentgraphics.co.uk>
2021-01-04 09:43:09 +0000

    Renew table of Win32-incompatible functions in Unix (#10116)

--- otherlibs/unix/unixLabels.mli
+++ otherlibs/unix/unixLabels.mli
@@ -13,6 +13,13 @@
 (*                                                                        *)
 (**************************************************************************)
 
+(* NOTE:
+   When a new function is added which is not implemented on Windows (or
+   partially implemented), or the Windows-status of an existing function is
+   changed, remember to update the summary table in
+   manual/manual/library/libunix.etex
+*)
+
 (** Interface to the Unix system.
 
    To use the labeled version of this module, add [module Unix][ = ][UnixLabels]
